8a629606a9ddddf1af766d80c660fad144264421
IKE1213 <ikedakoukun1213@gmail.com>
2026-06-08 13:26:03 +0900

    message change

--- main.py
+++ main.py
@@ -5,8 +5,4 @@ app = FastAPI()
 # GETリクエストで / にアクセスされたときの処理
 @app.get("/")
 def read_root():
-    return {"message": "CICD成功!やで"}
-
-@app.get("/hello")
-def read_hello():
-    return {"message": "Helloo, World"}
+    return {"message": "CICD成功!してるよ～"}
